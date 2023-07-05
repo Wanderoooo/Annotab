d3bd2d1d6211b66f7cf4a6e43a43dcde162dd5a5
Alissa Guo <guoalissa@gmail.com>
2023-07-05 22:05:55 +0800

    p5.js not working

---
+++ sketch.js
@@ -0,0 +1,14 @@
+
+document.addEventListener('DOMContentLoaded', () => {
+  const button = document.getElementById('draw');
+  var canvas;
+
+  function setup() {
+    canvas = createCanvas(windowWidth, windowHeight);
+    background(200);
+  }
+
+  button.addEventListener('click', () => {
+  })
+
+})
