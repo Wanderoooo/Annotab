72f217b4793d1496cce4573a697d7e1118f212dc
Alissa Guo <guoalissa@gmail.com>
2023-07-27 20:46:05 -0700

    added canvas v1, mouselistener

--- sketch.js
+++ sketch.js
@@ -1,14 +1,27 @@
+const button = document.getElementById('draw');
 
-document.addEventListener('DOMContentLoaded', () => {
-  const button = document.getElementById('draw');
-  var canvas;
+const s = ( sketch ) => {
 
-  function setup() {
-    canvas = createCanvas(windowWidth, windowHeight);
+  let x = 100;
+  let y = 100;
 
-  }
+  sketch.setup = () => {
+    document.body.style['userSelect'] = 'none';
+    let c = sketch.createCanvas(document.body.clientWidth, document.body.clientHeight);
+    c.position(0,0);
+    c.style('display', 'block');
+    c.style('z-index', '999');
+    sketch.clear();
+  };
 
-  button.addEventListener('click', () => {
-  })
+  sketch.draw = () => {
+    sketch.stroke(0);
+    sketch.strokeWeight(4);
+    if (sketch.mouseIsPressed) {
+      sketch.line(sketch.mouseX, sketch.mouseY, sketch.pmouseX, sketch.pmouseY);
+    }
+    
+  };
+};
 
-})
+let myp5 = new p5(s);
